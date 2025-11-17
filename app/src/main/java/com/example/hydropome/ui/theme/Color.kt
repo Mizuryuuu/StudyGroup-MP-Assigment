a1bc5a7aada2ea5fc514101f9cee494960820872
Munif <munif.syahli@gmail.com>
2025-11-17 19:31:04 +0700

    Menambahkan Homepage di Homepage.kt dan menambahkan beberapa color di Theme.Color.kt

--- app/src/main/java/com/example/hydropome/ui/theme/Color.kt
+++ app/src/main/java/com/example/hydropome/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Abuabu = Color(0xFFF4F5F7)
+
+val AbuabuText = Color(0xFF757575)
+
+val AbuabuTextSerch = Color(0xFF98A0AA)
+
+val HijauNormal = Color(0xFF179778)
