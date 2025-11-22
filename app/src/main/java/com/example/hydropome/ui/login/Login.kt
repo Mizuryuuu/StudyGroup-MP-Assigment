a3c6b1fa37a044c1c5e30f996d53b1093dc8e4fa
Laponn <naufal.sulthan.fakhry@gmail.com>
2025-11-22 11:05:38 +0700

    Pembuatan Login Page (Belum Selesai)

--- app/src/main/java/com/example/hydropome/ui/login/Login.kt
+++ app/src/main/java/com/example/hydropome/ui/login/Login.kt
@@ -4,16 +4,24 @@ package com.example.hydropome.ui.login
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 @Preview
@@ -24,14 +32,39 @@ fun Login(){
     Column (
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFFFFFFF))
+            .background(Color.White)
             .systemBarsPadding()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .padding(24.dp),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally,
 
 
     ){
+        IconButton(
+            onClick ={},
+            modifier = Modifier
+                .size(48.dp)
+                .offset(x = (-12).dp)
+        ){
+
+        }
+
+        Text(
+            text = "Selamat Datang Kembali",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+
+        Text(
+            text = "HydroPoMate! 🌿🍃",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
 
     }
 }
--- app/src/main/java/com/example/hydropome/ui/theme/Color.kt
+++ app/src/main/java/com/example/hydropome/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BrandGreen = Color(0xFF1B9A77)
+
+val IntBackground = Color(0xFFF5F6FA)
